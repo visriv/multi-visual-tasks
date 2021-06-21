d05665c03d1011ad2ad9cea8ca1dd6b327f48001
zhimingqian <zhimingqian@tencent.com>
2021-06-21 22:27:38 +0800

    update for yolov4x training

--- demos/det_infer_demo_retail.py
+++ demos/det_infer_demo_retail.py
@@ -34,10 +34,10 @@ if __name__ == '__main__':
     # task_config_path = 'task_settings/img_det/det_yolov4_retail_one.yaml'
     # checkpoint_path = 'meta/train_infos/det_yolov4_retail_one/epoch_200.pth'
     task_config_path = 'task_settings/img_det/det_yolov4x_cspdarknet_retail_one.yaml'
-    checkpoint_path = 'meta/train_infos/det_yolov4x_cspdarknet_retail_one/epoch_60.pth'
+    checkpoint_path = 'meta/train_infos/det_yolov4x_cspdarknet_retail_one/epoch_100.pth'
 
     with_show = True
-    show_score_thr = 0.01
+    show_score_thr = 0.3
 
     print(
         'Infer the detection results from image with index {}.'.format(index))
