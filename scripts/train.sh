c0e2201de547ac7a745a343304edd0e68f1320de
Jiaolong <jiaolongxu@.gmail.com>
2021-06-22 11:28:24 +0800

    added 9 anchors task config

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,2 @@
 python3 ./tools/train.py --work-dir meta/train_infos --no-test \
-    --load-from meta/train_infos/det_yolov4_retail_one_v0/epoch_200.pth \
-    task_settings/img_det/det_yolov4_retail_one.yaml 
+    task_settings/img_det/det_yolov4_9a_retail_one.yaml 
