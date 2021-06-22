ba0919b4e7159c53c8575cd22683c533f29141d4
zhimingqian <zhimingqian@tencent.com>
2021-06-22 12:28:10 +0800

    update for middle size detection

--- demos/det_infer_demo_retail.py
+++ demos/det_infer_demo_retail.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     # task_config_path = 'task_settings/img_det/det_yolov4_retail_one.yaml'
     # checkpoint_path = 'meta/train_infos/det_yolov4_retail_one/epoch_200.pth'
     task_config_path = 'task_settings/img_det/det_yolov4x_cspdarknet_retail_one.yaml'
-    checkpoint_path = 'meta/train_infos/det_yolov4x_cspdarknet_retail_one/epoch_100.pth'
+    checkpoint_path = 'meta/train_infos/det_yolov4x_cspdarknet_retail_one/epoch_52.pth'
 
     with_show = True
     show_score_thr = 0.3
