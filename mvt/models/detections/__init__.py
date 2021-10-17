e335b0d9814c8dd5f6298b59c3f94352d8c5741d
zhimingqian <zhimingqian@tencent.com>
2021-10-17 17:29:48 +0800

    update for remove detectors

--- mvt/models/detections/__init__.py
+++ mvt/models/detections/__init__.py
@@ -1,28 +1,21 @@
-from .base_detectors import BaseDetector, SingleStageDetector, TwoStageDetector
-from .atss import ATSS
+from .base_detectors import SingleStageDetector, TwoStageDetector
 from .cascade_rcnn import CascadeRCNN
 from .cornernet import CornerNet
-from .fast_rcnn import FastRCNN
 from .faster_rcnn import FasterRCNN
 from .fcos import FCOS
-from .fovea import FOVEA
 from .mask_rcnn import MaskRCNN
 from .retinanet import RetinaNet
 from .ssd import SSD
 from .yolo import YOLOV3, YOLOV4, YOLOV5
 
 __all__ = [
-    "BaseDetector",
     "SingleStageDetector",
     "TwoStageDetector",
-    "ATSS",
     "SSD",
     "CascadeRCNN",
     "CornerNet",
-    "FastRCNN",
     "FasterRCNN",
     "FCOS",
-    "FOVEA",
     "MaskRCNN",
     "RetinaNet",
     "SSD",
